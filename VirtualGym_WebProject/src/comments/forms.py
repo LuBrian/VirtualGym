cc1fee1830f9f111dee7cb95fd6501b4982dcae8
pang <383066079@qq.com>
2017-10-13 20:57:24 -0600

    add user comment back end finish and front end design

---
+++ VirtualGym_WebProject/src/comments/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from .models import Comment
+
+
+class CommentForm(forms.ModelForm):
+    content=forms.CharField(widget=forms.TextInput(
+        attrs={
+            "class":"form-control",
+            "placeholder":"your comments...",
+        }
+    ))
+    # parent_id=forms.IntegerField(widget=forms.HiddenInput,required=False)
+    class Meta:
+        model=Comment
+        # fields={"exerciseDescription","exerciseTag"}
+        fields={"content"}
