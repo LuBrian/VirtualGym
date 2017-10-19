3d38957eda537fb73a0a90d25eae86f2680c3116
Xin <richardhuang1004@gmail.com>
2017-10-18 21:37:55 -0600

    update Q&A forum(style and connected to database)

--- VirtualGym_WebProject/src/forum/models.py
+++ VirtualGym_WebProject/src/forum/models.py
@@ -4,7 +4,7 @@ from users.models import MyUsers
 # Create your models here.
 class Questions(models.Model):
 	questionID = models.AutoField(primary_key=True)
-	questionDescription = models.CharField(max_length=1000)
+	questionDescription = models.CharField(null = False, blank = False, max_length=1000)
 	# exerciseID = models.ForeignKey('Exercise', on_delete=models.CASCADE)
 	userID = models.ForeignKey(MyUsers, on_delete=models.CASCADE)
 	dateAsked = models.DateTimeField(auto_now_add=True)
