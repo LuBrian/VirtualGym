f1a1f48690c4fb4fcafc0d0e118629e1fe43e4e3
LuBrian <blu1@ualberta.ca>
2017-10-20 19:09:44 -0600

    fix first visit notification on Sign Up page

--- VirtualGym_WebProject/src/users/views.py
+++ VirtualGym_WebProject/src/users/views.py
@@ -63,7 +63,6 @@ def signUp(request):
 	title="Sign Up"
 	form= SignUpForm(request.POST or None)
 	exception = "None"
-	
 
 	context={
 		"title":title,
@@ -71,23 +70,26 @@ def signUp(request):
 		"page_title":title,
 		"exception": exception
 	}
-	try:
-		form.is_valid()
-		instance=form.save(commit=False)
-		instance.save()
-		user = authenticate(email=instance.email,password=instance.password)
-		login(request, user)
-		return redirect('index')
-	except Exception as e:
-		print '%s (%s)' % (e.message, type(e))
-		context["exception"] = e.message
-		# print(context["exception"])
-		# print(context)
-	
+
+	if request.method == 'POST':
+		try:
+			form.is_valid()
+			instance=form.save(commit=False)
+			instance.save()
+			user = authenticate(email=instance.email,password=instance.password)
+			login(request, user)
+			return redirect('index')
+		except Exception as e:
+			print ('%s (%s)' % (e.message, type(e)))
+			context["exception"] = e.message
+			# print(context["exception"])
+			# print(context)
+		
+		return render(request,"signUp.html",context)
+
+
+
 	return render(request,"signUp.html",context)
-
-
-	# return render(request,"signUp.html",context)
 		
 
 
