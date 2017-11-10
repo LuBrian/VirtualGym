4e498ee27458d55a0771b593ff5918ee34e4e14d
LuBrian <blu1@ualberta.ca>
2017-11-09 23:08:32 -0700

    finish adding video annotation frontend, backend

--- VirtualGym_WebProject/src/annotations/views.py
+++ VirtualGym_WebProject/src/annotations/views.py
@@ -5,34 +5,47 @@ from django.shortcuts import render,get_object_or_404
 from exercise.models import Videos
 # from .forms import AnnotationForm
 from django.views.decorators.csrf import csrf_exempt,csrf_protect
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
 from .models import Annotation
 import json
 # Create your views here.
 
 @csrf_exempt #This skips csrf validation. Use csrf_protect to have validation
 def createAnnotation(request,vidID):
-	print('haha')
+
 
 	instance=get_object_or_404(Videos,video_id=vidID)
 	title="Add annotations"
 
+	annotationSet = Annotation.objects.filter(toVid=vidID)
+	dataSet = []
+	for element in annotationSet:
+		dataSet.append(element.details);
+	json_string = json.dumps(dataSet)
 	
-	print('before post')
+	
+
 
 	if request.is_ajax():
 		print('in ajax')
-		JSONdata = request.body
-		# tempDict = simplejson.JSONDecoder().decode( JSONdata ) 
-		data_dict = json.loads(JSONdata)[0]
-		tempAnnotation = Annotation(toVid=instance, details=data_dict["details"], x = data_dict["x"],y=data_dict["y"],timeStep = data_dict["time"])
-		tempAnnotation.save()
-		return HttpResponse("OK")
+		if request.method == "POST":
+			print("in post ajax")
+			print('in ajax')
+			JSONdata = request.body
+			tempAnnotation = Annotation(toVid=instance, details=JSONdata)
+			tempAnnotation.save()
+			return HttpResponse("OK")
 
+		if request.method == "GET":
+			print('in get ajax')
+
+			return HttpResponse(json_string)
 
 	context = {
 		"title":title,
 		"instance":instance,
+		"contents" :json_string,
 	}
+	
 
 	return render(request,"addAnnotation.html",context)
