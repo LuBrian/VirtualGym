d8d4b74ff49f6fff96c70c001e5c77b06dc00614
pang <383066079@qq.com>
2017-11-24 20:16:55 -0700

    search ui

--- VirtualGym_WebProject/src/static/js/jquery.js
+++ VirtualGym_WebProject/src/static/js/jquery.js
@@ -1,10 +1,11 @@
 
-//function for hide replay 
-function myFunction(btn) {
-    var x = document.getElementById(btn);
-    if (x.style.display === "block") {
+//function for hide replay
+function myFunction() {
+    var x = document.getElementById('Search');
+    if (x.style.display == "block") {
         x.style.display = "none";
     } else {
         x.style.display = "block";
     }
 }
+//<button id="button" style="float: right;" type="button" class="btn btn-info btn-sm" onclick="myFunction({{obj.questionID}})">
