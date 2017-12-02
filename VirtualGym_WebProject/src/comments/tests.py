598c6236f44e20712547fe29e4c807bf7dffce8c
Xin <richardhuang1004@gmail.com>
2017-12-01 19:55:09 -0700

    all tests

--- VirtualGym_WebProject/src/comments/tests.py
+++ VirtualGym_WebProject/src/comments/tests.py
@@ -6,46 +6,45 @@ import time
 import os,sys
 
 # this test is a UI test for comment app
-# class UITestCase(TestCase):
-# 	# set up browser to firefox browser
-#     def setUp(self):
-#         self.browser = webdriver.Firefox()
+class UITestCase(TestCase):
+	# set up browser to firefox browser
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-#     close browser after test    
-#     def tearDown(self):
-#         self.browser.quit()
+    # close browser after test    
+	def tearDown(self):
+		self.browser.quit()
 
-#     # test if user can comment a exercise
-#     # test if user can reply a comment
-#     def test_add_comment(self):
-#         try:
-#             self.browser.get("http://localhost:8000/viewProfile/")
-#             self.assertIn("http://localhost:8000/viewProfile/", self.browser.current_url)
-#             title = self.browser.find_element_by_partial_link_text('test').text
-#             self.assertEqual('test' in title, True)
-#             title = self.browser.find_element_by_partial_link_text('test').click()
-#             # signIn = self.browser.find_elements_by_xpath("/html/body/div/div[3]/div/div[3]/form/a").click()
-#             if(self.browser.find_element_by_partial_link_text('Sign In')):
-#                 signIn = self.browser.find_element_by_partial_link_text('Sign In').click()
-#                 email = self.browser.find_element_by_id('email')
-#                 email.send_keys('sprint4_test@vg.ca')
-#                 password = self.browser.find_element_by_id('password')
-#                 password.send_keys('123')
-#                 sign_In = self.browser.find_element_by_id('signInButton').click()
-#                 check = self.browser.find_element_by_id("detail_title").text
-#                 self.assertEqual('test' in check, True)
-        	
-        		
-#             comment = self.browser.find_element_by_name('comment')
-#             comment.send_keys("comment from test")
-#             comment.click()
-#             submit_comment = self.browser.find_element_by_id("submitComment").click()
-#             reply = self.browser.find_elements_by_xpath("//*[@id='reply_click']")
-#             reply[0].click()
-#             reply = self.browser.find_elements_by_xpath("//*[@id='id_comment']")
-#             reply[1].send_keys("reply from test")
-#             submit_reply = self.browser.find_element_by_id("reply").click()
-#             print("Comment test passed!")
-#         except:
-#             print("Comment test failed! server down or no internet for now")
-#             print("Please check or contact us! ")
+    # test if user can comment a exercise
+    # test if user can reply a comment
+    def test_add_comment(self):
+        try:
+	        self.browser.get("http://localhost:8000/viewProfile/")
+	        self.assertIn("http://localhost:8000/viewProfile/", self.browser.current_url)
+	        title = self.browser.find_element_by_partial_link_text('Side').text
+	        self.assertEqual('Side' in title, True)
+	        title = self.browser.find_element_by_partial_link_text('Side').click()
+	        if(self.browser.find_element_by_partial_link_text('Sign In')):
+	            signIn = self.browser.find_element_by_partial_link_text('Sign In').click()
+	            email = self.browser.find_element_by_id('email')
+	            email.send_keys('sprint4_test@vg.ca')
+	            password = self.browser.find_element_by_id('password')
+	            password.send_keys('123')
+	            sign_In = self.browser.find_element_by_id('signInButton').click()
+	            check = self.browser.find_element_by_id("detail_title").text
+	            self.assertEqual('Side' in check, True)
+	        	
+	        		
+	        comment = self.browser.find_element_by_name('comment')
+	        comment.send_keys("comment from test")
+	        comment.click()
+	        submit_comment = self.browser.find_element_by_id("submitComment").click()
+	        reply = self.browser.find_elements_by_xpath("//*[@id='reply_click']")
+	        reply[0].click()
+	        reply = self.browser.find_elements_by_xpath("//*[@id='id_comment']")
+	        reply[1].send_keys("reply from test")
+	        submit_reply = self.browser.find_element_by_id("reply").click()
+	        print("Comment test passed!")
+        except:
+            print("Comment test failed! server down or no internet for now")
+            print("Please check or contact us! ")
--- VirtualGym_WebProject/src/users/tests.py
+++ VirtualGym_WebProject/src/users/tests.py
@@ -11,166 +11,169 @@ class UITestCase(TestCase):
 	def setUp(self):
 		self.browser = webdriver.Firefox()
 
-#     # close the browser after test
-# 	def tearDown(self):
-# 		self.browser.quit()
+    # close the browser after test
+	def tearDown(self):
+		self.browser.quit()
 
-#     # test if user can signIn with their vg account through type
-#     # in their username and password    
-# 	def test_signIn(self):
-# 		try:
-# 			self.browser.get("http://localhost:8000/")
-# 			self.assertIn("http://localhost:8000/", self.browser.current_url)
-# 			signIn = self.browser.find_element_by_xpath("//*[@id='signIn_popup_trigger']").click()
-# 			email = self.browser.find_element_by_id('email')
-# 			email.send_keys('sprint4_test@vg.ca')
-# 			password = self.browser.find_element_by_id('password')
-# 			password.send_keys('123')
-# 			signIn = self.browser.find_element_by_id('signInButton').click()
-# 			self.assertIn("http://localhost:8000/", self.browser.current_url)
-# 			print("User sign in test passed!")
-# 		except:
-# 			print("Sign in test failed! server down or no internet for now")
-# 			print("Please check or contact us! ")
+    # test if user can signIn with their vg account through type
+    # in their username and password    
+	def test_signIn(self):
+		try:
+			self.browser.get("http://localhost:8000/")
+			self.assertIn("http://localhost:8000/", self.browser.current_url)
+			signIn = self.browser.find_element_by_xpath("//*[@id='signIn_popup_trigger']").click()
+			email = self.browser.find_element_by_id('email')
+			email.send_keys('sprint4_test@vg.ca')
+			password = self.browser.find_element_by_id('password')
+			password.send_keys('123')
+			signIn = self.browser.find_element_by_id('signInButton').click()
+			self.assertIn("http://localhost:8000/", self.browser.current_url)
+			print("User sign in test passed!")
+		except:
+			print("Sign in test failed! server down or no internet for now")
+			print("Please check or contact us! ")
 
-#     # test if user can sign out through navgation bar
-# 	def test_signOut(self):
-# 		try:
-# 			self.browser.get("http://localhost:8000/")
-# 			self.assertIn("http://localhost:8000/", self.browser.current_url)
-# 			signIn = self.browser.find_element_by_xpath("//*[@id='signIn_popup_trigger']").click()
-# 			email = self.browser.find_element_by_id('email')
-# 			email.send_keys('sprint4_test@vg.ca')
-# 			password = self.browser.find_element_by_id('password')
-# 			password.send_keys('123')
-# 			signIn = self.browser.find_element_by_id('signInButton').click()
-# 			self.assertIn("http://localhost:8000/", self.browser.current_url)
-# 			signOut = self.browser.find_element_by_xpath('/html/body/div/div[1]/nav/div/div[2]/ul/li[6]/a').click()
-# 			check = self.browser.find_element_by_xpath("//*[@id='signIn_popup_trigger']")
-# 			self.assertTrue(check)
-# 			print("User sign out test passed!")
-# 		except:
-# 			print("Sign out test failed! server down or no internet for now")
-# 			print("Please check or contact us! ")
+    # test if user can sign out through navgation bar
+	def test_signOut(self):
+		try:
+			self.browser.get("http://localhost:8000/")
+			self.assertIn("http://localhost:8000/", self.browser.current_url)
+			signIn = self.browser.find_element_by_xpath("//*[@id='signIn_popup_trigger']").click()
+			email = self.browser.find_element_by_id('email')
+			email.send_keys('sprint4_test@vg.ca')
+			password = self.browser.find_element_by_id('password')
+			password.send_keys('123')
+			signIn = self.browser.find_element_by_id('signInButton').click()
+			self.assertIn("http://localhost:8000/", self.browser.current_url)
+			signOut = self.browser.find_element_by_xpath('/html/body/div/div[1]/nav/div/div[2]/ul/li[6]/a').click()
+			check = self.browser.find_element_by_xpath("//*[@id='signIn_popup_trigger']")
+			self.assertTrue(check)
+			print("User sign out test passed!")
+		except:
+			print("Sign out test failed! server down or no internet for now")
+			print("Please check or contact us! ")
 
-#     # test if user can sign in through their google account
-# 	def test_google_SignIn(self):
-# 		try:
-# 			self.browser.get("http://localhost:8000/")
-# 			self.assertIn("http://localhost:8000/", self.browser.current_url)
-# 			signIn = self.browser.find_element_by_xpath("//*[@id='signIn_popup_trigger']").click()
-# 			Google_signIn = self.browser.find_element_by_xpath('//*[@id="googleSignIn"]').click()
-# 			time.sleep(1)
-# 			email = self.browser.find_element_by_xpath("//*[@id='identifierId']")
-# 			email.send_keys("virthalgymlogintest@gmail.com")
-# 			time.sleep(1)
-# 			next_step = self.browser.find_element_by_xpath("//*[@id='identifierNext']/content/span").click()
-# 			time.sleep(3)
-# 			password = self.browser.find_element_by_xpath("//*[@id='password']/div[1]/div/div[1]/input")
-# 			password.send_keys("virtualgym")
-# 			time.sleep(1)
-# 			password_step = self.browser.find_element_by_xpath("//*[@id='passwordNext']/content/span").click()
-# 			time.sleep(1)
-# 			self.assertIn("http://localhost:8000/", self.browser.current_url)
-# 			print("Google sign in test passed!")
-# 		except:
-# 			print("Google sign in test failed! server down or no internet for now")
-# 			print("Please check or contact us! ")
+    # test if user can sign in through their google account
+	def test_google_SignIn(self):
+		try:
+			self.browser.get("http://localhost:8000/")
+			self.assertIn("http://localhost:8000/", self.browser.current_url)
+			signIn = self.browser.find_element_by_xpath("//*[@id='signIn_popup_trigger']").click()
+			Google_signIn = self.browser.find_element_by_xpath('//*[@id="googleSignIn"]').click()
+			time.sleep(1)
+			email = self.browser.find_element_by_xpath("//*[@id='identifierId']")
+			email.send_keys("virthalgymlogintest@gmail.com")
+			time.sleep(1)
+			next_step = self.browser.find_element_by_xpath("//*[@id='identifierNext']/content/span").click()
+			time.sleep(3)
+			password = self.browser.find_element_by_xpath("//*[@id='password']/div[1]/div/div[1]/input")
+			password.send_keys("virtualgym")
+			time.sleep(1)
+			password_step = self.browser.find_element_by_xpath("//*[@id='passwordNext']/content/span").click()
+			time.sleep(1)
+			self.assertIn("http://localhost:8000/", self.browser.current_url)
+			print("Google sign in test passed!")
+		except:
+			print("Google sign in test failed! server down or no internet for now")
+			print("Please check or contact us! ")
 
 
-#     # test if moderator can login through admin page by type in
-#     # their superUser username and password
-# 	def test_moderator_Login(self):
-# 		try:
-# 			self.browser.get("http://localhost:8000/admin/")
-# 			self.assertIn("http://localhost:8000/admin/", self.browser.current_url)
-# 			username = self.browser.find_element_by_xpath("//*[@id='id_username']")
-# 			username.send_keys("sprint4_admin@vg.ca")
-# 			password = self.browser.find_element_by_xpath("//*[@id='id_password']")
-# 			password.send_keys("admin1234")
-# 			Log_in = self.browser.find_element_by_xpath("//*[@id='login-form']/div[3]/input").click()
-# 			self.assertIn("http://localhost:8000/admin/", self.browser.current_url)
-# 			print("Moderator login test passed!")
-# 		except:
-# 			print("Moderator login test failed! server down or no internet for now")
-# 			print("Please check or contact us! ")
+    # test if moderator can login through admin page by type in
+    # their superUser username and password
+	def test_moderator_Login(self):
+		try:
+			self.browser.get("http://localhost:8000/admin/")
+			self.assertIn("http://localhost:8000/admin/", self.browser.current_url)
+			username = self.browser.find_element_by_xpath("//*[@id='id_username']")
+			username.send_keys("sprint4_admin@vg.ca")
+			password = self.browser.find_element_by_xpath("//*[@id='id_password']")
+			password.send_keys("admin1234")
+			Log_in = self.browser.find_element_by_xpath("//*[@id='login-form']/div[3]/input").click()
+			self.assertIn("http://localhost:8000/admin/", self.browser.current_url)
+			print("Moderator login test passed!")
+		except:
+			print("Moderator login test failed! server down or no internet for now")
+			print("Please check or contact us! ")
 
-#     # test if user can sign in through their facebook account
-# 	def test_facebook_SignIn(self):
-# 		try:
-# 			self.browser.get("http://localhost:8000/")
-# 			self.assertIn("http://localhost:8000/", self.browser.current_url)
-# 			signIn = self.browser.find_element_by_xpath("//*[@id='signIn_popup_trigger']").click()
-# 			facebook_signIn = self.browser.find_element_by_xpath('//*[@id="facebookSignIn"]').click()
-# 			time.sleep(1)
-# 			email = self.browser.find_element_by_xpath("//*[@id='email']")
-# 			email.send_keys("virthalgymlogintest@gmail.com")
-# 			time.sleep(1)
-# 			password = self.browser.find_element_by_xpath("//*[@id='pass']")
-# 			password.send_keys("virtualgym")
-# 			time.sleep(1)
-# 			logIn = self.browser.find_element_by_xpath("//*[@id='loginbutton']").click()
-# 			try:
-# 				continueLogin = self.browser.find_element_by_xpath("//*[@id='u_0_x']/div[2]/div[1]/div[1]/button").click()
-# 			except:
-# 				print("user already signed Up with this facebook account")
-# 				print("Log in successful!")
+    # test if user can sign in through their facebook account
+	def test_facebook_SignIn(self):
+		try:
+			self.browser.get("http://localhost:8000/")
+			self.assertIn("http://localhost:8000/", self.browser.current_url)
+			signIn = self.browser.find_element_by_xpath("//*[@id='signIn_popup_trigger']").click()
+			facebook_signIn = self.browser.find_element_by_xpath('//*[@id="facebookSignIn"]').click()
+			time.sleep(1)
+			email = self.browser.find_element_by_xpath("//*[@id='email']")
+			email.send_keys("virthalgymlogintest@gmail.com")
+			time.sleep(1)
+			password = self.browser.find_element_by_xpath("//*[@id='pass']")
+			password.send_keys("virtualgym")
+			time.sleep(1)
+			logIn = self.browser.find_element_by_xpath("//*[@id='loginbutton']").click()
+			try:
+				continueLogin = self.browser.find_element_by_xpath("//*[@id='u_0_x']/div[2]/div[1]/div[1]/button").click()
+			except:
+				print("user already signed Up with this facebook account")
+				print("Log in successful!")
 
-# 			time.sleep(3)
-# 			self.assertIn("http://localhost:8000/", self.browser.current_url)
-# 			print("Facebook sign in test passed!")
-# 		except:
-# 			print("Facebook sign in test failed! server down or no internet for now")
-# 			print("Please check or contact us! ")
+			time.sleep(3)
+			self.assertIn("http://localhost:8000/", self.browser.current_url)
+			print("Facebook sign in test passed!")
+		except:
+			print("Facebook sign in test failed! server down or no internet for now")
+			print("Please check or contact us! ")
 
-#     # test if user can sign in through their twitter account
-# 	def test_twitter_SignIn(self):
-# 		try:
-# 			self.browser.get("http://localhost:8000/")
-# 			self.assertIn("http://localhost:8000/", self.browser.current_url)
-# 			signIn = self.browser.find_element_by_xpath("//*[@id='signIn_popup_trigger']").click()
-# 			twitter_signIn = self.browser.find_element_by_xpath('//*[@id="twitterSignIn"]').click()
-# 			time.sleep(1)
-# 			email = self.browser.find_element_by_xpath("//*[@id='username_or_email']")
-# 			email.send_keys("virthalgymlogintest@gmail.com")
-# 			time.sleep(1)
-# 			password = self.browser.find_element_by_xpath("//*[@id='password']")
-# 			password.send_keys("virtualgym")
-# 			time.sleep(1)
-# 			logIn = self.browser.find_element_by_xpath("//*[@id='allow']").click()
-# 			time.sleep(3)
-# 			self.assertIn("http://localhost:8000/", self.browser.current_url)
-# 			print("Twitter sign in test passed!")
-# 		except:
-# 			print("Twitter sign in test failed! server down or no internet for now")
-# 			print("Please check or contact us! ")
-	# def test_give_moderator_privileges(self):
-	# 	try:
-	# 		self.browser.get("http://localhost:8000/admin/")
-	# 		self.assertIn("http://localhost:8000/admin/", self.browser.current_url)
-	# 		username = self.browser.find_element_by_xpath("//*[@id='id_username']")
-	# 		username.send_keys("sprint4_admin@vg.ca")
-	# 		password = self.browser.find_element_by_xpath("//*[@id='id_password']")
-	# 		password.send_keys("admin1234")
-	# 		Log_in = self.browser.find_element_by_xpath("//*[@id='login-form']/div[3]/input").click()
-	# 		self.browser.find_element_by_xpath("//*[@id='menu-content']/li[8]/i/a").click()
-	# 		self.browser.find_element_by_xpath("//*[@id='result_list']/tbody/tr[5]/th/a").click()
-	# 		self.browser.find_element_by_xpath("//*[@id='id_is_admin']").click()
-	# 		self.browser.find_element_by_xpath("//*[@id='id_is_superuser']").click()
-	# 		self.browser.find_element_by_xpath("//*[@id='myusers_form']/div/div/input[1]").click()
-	# 		self.browser.find_element_by_xpath("//*[@id='result_list']/tbody/tr[5]/th/a").click()
-	# 		admin_box = self.browser.find_element_by_xpath("//*[@id='id_is_admin']").is_selected()
-	# 		self.assertTrue(admin_box == True)
-	# 		super_box = self.browser.find_element_by_xpath("//*[@id='id_is_superuser']").is_selected()
-	# 		self.assertTrue(super_box == True)
-	# 		self.browser.find_element_by_xpath("//*[@id='myusers_form']/div/div/input[1]").click()
-	# 		# everything goes ok, cancel privileges
-	# 		self.browser.find_element_by_xpath("//*[@id='result_list']/tbody/tr[5]/th/a").click()
-	# 		self.browser.find_element_by_xpath("//*[@id='id_is_admin']").click()
-	# 		self.browser.find_element_by_xpath("//*[@id='id_is_superuser']").click()
-	# 		self.browser.find_element_by_xpath("//*[@id='myusers_form']/div/div/input[1]").click()
-	# 		print("Give moderator privileges test passed!")
-	# 	except:
-	# 		print("Give moderator privileges test failed! server down or no internet for now")
-	# 		print("Please check or contact us! ")
+    # test if user can sign in through their twitter account
+	def test_twitter_SignIn(self):
+		try:
+			self.browser.get("http://localhost:8000/")
+			self.assertIn("http://localhost:8000/", self.browser.current_url)
+			signIn = self.browser.find_element_by_xpath("//*[@id='signIn_popup_trigger']").click()
+			twitter_signIn = self.browser.find_element_by_xpath('//*[@id="twitterSignIn"]').click()
+			time.sleep(1)
+			email = self.browser.find_element_by_xpath("//*[@id='username_or_email']")
+			email.send_keys("virthalgymlogintest@gmail.com")
+			time.sleep(1)
+			password = self.browser.find_element_by_xpath("//*[@id='password']")
+			password.send_keys("virtualgym")
+			time.sleep(1)
+			logIn = self.browser.find_element_by_xpath("//*[@id='allow']").click()
+			time.sleep(3)
+			self.assertIn("http://localhost:8000/", self.browser.current_url)
+			print("Twitter sign in test passed!")
+		except:
+			print("Twitter sign in test failed! server down or no internet for now")
+			print("Please check or contact us! ")
+
+	# test if we can give moderator privileges to an account
+	def test_give_moderator_privileges(self):
+		try:
+			self.browser.get("http://localhost:8000/admin/")
+			self.assertIn("http://localhost:8000/admin/", self.browser.current_url)
+			username = self.browser.find_element_by_xpath("//*[@id='id_username']")
+			username.send_keys("sprint4_admin@vg.ca")
+			password = self.browser.find_element_by_xpath("//*[@id='id_password']")
+			password.send_keys("admin1234")
+			Log_in = self.browser.find_element_by_xpath("//*[@id='login-form']/div[3]/input").click()
+			self.browser.find_element_by_xpath("//*[@id='menu-content']/li[8]/i/a").click()
+			self.browser.find_element_by_xpath("//*[@id='result_list']/tbody/tr[5]/th/a").click()
+			self.browser.find_element_by_xpath("//*[@id='id_is_admin']").click()
+			self.browser.find_element_by_xpath("//*[@id='id_is_superuser']").click()
+			self.browser.find_element_by_xpath("//*[@id='myusers_form']/div/div/input[1]").click()
+			self.browser.find_element_by_xpath("//*[@id='result_list']/tbody/tr[5]/th/a").click()
+
+			admin_box = self.browser.find_element_by_xpath("//*[@id='id_is_admin']").is_selected()
+			self.assertTrue(admin_box == True)
+			super_box = self.browser.find_element_by_xpath("//*[@id='id_is_superuser']").is_selected()
+			self.assertTrue(super_box == True)
+			self.browser.find_element_by_xpath("//*[@id='myusers_form']/div/div/input[1]").click()
+			# everything goes ok, cancel privileges
+			self.browser.find_element_by_xpath("//*[@id='result_list']/tbody/tr[5]/th/a").click()
+			self.browser.find_element_by_xpath("//*[@id='id_is_admin']").click()
+			self.browser.find_element_by_xpath("//*[@id='id_is_superuser']").click()
+			self.browser.find_element_by_xpath("//*[@id='myusers_form']/div/div/input[1]").click()
+			print("Give moderator privileges test passed!")
+		except:
+			print("Give moderator privileges test failed! server down or no internet for now")
+			print("Please check or contact us! ")
 
