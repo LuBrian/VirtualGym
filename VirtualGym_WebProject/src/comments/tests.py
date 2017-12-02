1921afec5989951fcd1d7aabd5e8a4f6b066ba3e
Xin <richardhuang1004@gmail.com>
2017-12-01 20:06:17 -0700

    all test cases

--- VirtualGym_WebProject/src/comments/tests.py
+++ VirtualGym_WebProject/src/comments/tests.py
@@ -17,34 +17,33 @@ class UITestCase(TestCase):
 
 	# test if user can comment a exercise
 	# test if user can reply a comment
-    def test_add_comment(self):
-        try:
-	        self.browser.get("http://localhost:8000/viewProfile/")
-	        self.assertIn("http://localhost:8000/viewProfile/", self.browser.current_url)
-	        title = self.browser.find_element_by_partial_link_text('Side').text
-	        self.assertEqual('Side' in title, True)
-	        title = self.browser.find_element_by_partial_link_text('Side').click()
-	        if(self.browser.find_element_by_partial_link_text('Sign In')):
-	            signIn = self.browser.find_element_by_partial_link_text('Sign In').click()
-	            email = self.browser.find_element_by_id('email')
-	            email.send_keys('sprint4_test@vg.ca')
-	            password = self.browser.find_element_by_id('password')
-	            password.send_keys('123')
-	            sign_In = self.browser.find_element_by_id('signInButton').click()
-	            check = self.browser.find_element_by_id("detail_title").text
-	            self.assertEqual('Side' in check, True)
-	        	
-	        		
-	        comment = self.browser.find_element_by_name('comment')
-	        comment.send_keys("comment from test")
-	        comment.click()
-	        submit_comment = self.browser.find_element_by_id("submitComment").click()
-	        reply = self.browser.find_elements_by_xpath("//*[@id='reply_click']")
-	        reply[0].click()
-	        reply = self.browser.find_elements_by_xpath("//*[@id='id_comment']")
-	        reply[1].send_keys("reply from test")
-	        submit_reply = self.browser.find_element_by_id("reply").click()
-	        print("Comment test passed!")
-        except:
-            print("Comment test failed! server down or no internet for now")
-            print("Please check or contact us! ")
+	def test_add_comment(self):
+		try:
+			self.browser.get("http://localhost:8000/viewProfile/")
+			self.assertIn("http://localhost:8000/viewProfile/", self.browser.current_url)
+			title = self.browser.find_element_by_partial_link_text('Side').text
+			self.assertEqual('Side' in title, True)
+			title = self.browser.find_element_by_partial_link_text('Side').click()
+			if(self.browser.find_element_by_partial_link_text('Sign In')):
+				signIn = self.browser.find_element_by_partial_link_text('Sign In').click()
+				email = self.browser.find_element_by_id('email')
+				email.send_keys('sprint4_test@vg.ca')
+				password = self.browser.find_element_by_id('password')
+				password.send_keys('123')
+				sign_In = self.browser.find_element_by_id('signInButton').click()
+				check = self.browser.find_element_by_id("detail_title").text
+				self.assertEqual('Side' in check, True)
+
+			comment = self.browser.find_element_by_name('comment')
+			comment.send_keys("comment from test")
+			comment.click()
+			submit_comment = self.browser.find_element_by_id("submitComment").click()
+			reply = self.browser.find_elements_by_xpath("//*[@id='reply_click']")
+			reply[0].click()
+			reply = self.browser.find_elements_by_xpath("//*[@id='id_comment']")
+			reply[1].send_keys("reply from test")
+			submit_reply = self.browser.find_element_by_id("reply").click()
+			print("Comment test passed!")
+		except:
+			print("Comment test failed! server down or no internet for now")
+			print("Please check or contact us! ")
